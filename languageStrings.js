74da593cf0fa2db8216aab68196db5753fcb4da1
mitchburnett <euphamisim@gmail.com>
2018-02-03 22:24:18 -0700

    initial commit of working alexa app

---
+++ languageStrings.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+	languageStrings : {
+		'en' : {
+			'translation' : {
+				'SKILL_NAME' : 'Overwatch League',
+				'WELCOME_MSG' : 'Welcome to Overwatch League. Ask about Overwatch League teams, schedule, and ranking.',
+				'INVALID_TEAM_MSG' : 'I am sorry, %s is not a valid team.',
+				'TEAM_REPROMPT' : 'Can I help by searching for another team?',
+				'API_ERROR_MSG' : 'Something went wrong, please try again later.',
+				'HELP_MSG' : 'I can help you find information on Overwatch League team schedules.',
+				'SHUTDOWN_MSG' : 'The world could always use more heros.'
+			}
+		},
+		'en-US' : {
+			'translation' : {
+				'SKILL_NAME' : 'Overwatch League',
+				'WELCOME_MSG' : 'Welcome to Overwatch League. Ask about Overwatch League teams, schedule, and ranking.',
+				'INVALID_TEAM_MSG' : 'I am sorry, %s is not a valid team.',
+				'TEAM_REPROMPT' : 'Can I help by searching for another team?',
+				'API_ERROR_MSG' : 'Something went wrong, please try again later.',
+				'HELP_MSG' : 'I can help you find information on Overwatch League team schedules.',
+				'SHUTDOWN_MSG' : 'The world could always use more heros.'
+			}
+		}	 
+	}
+};
