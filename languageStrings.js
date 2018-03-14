211e6bf304cc5b7948166860965e20b680da5b23
mitchburnett <euphamisim@gmail.com>
2018-03-13 23:33:00 -0700

    Modified help messages to include the new commands for getTodaysMatches, getYesterdaysMatches and getTomorrowsMatches. Modified the help message to direct users to ask for help to hear a longer explanation of skill commands. Added a HELP_REPROMPT. It seemed to be missing.

--- languageStrings.js
+++ languageStrings.js
@@ -5,12 +5,14 @@ module.exports = {
 			'translation' : {
 				'SKILL_NAME' : 'Overwatch League',
 				'WELCOME_MSG' : 'Welcome to Overwatch League. You can ask me about Overwatch League games and standings. For example,\
-				 you can ask when the next %s game is, or, who the top three teams are.',
+				 you can ask when the next %s game is, or, who the top three teams are. You can also ask me what games are today, or,\
+				 what yesterday\'s results were. I am also able to tell you the games that will be played tomorrow. For more help,\
+				 say help.',
 
 				'WELCOME_REPROMPT' : 'For match information say, when is the next %s game, or, for ranking information ask what are\
-				 the league standings.',
+				 the league standings. For more help, say help.',
 
-				'PERMISSIONS_WELCOME' : 'Welcome to Overwatch League. You have asked about a next match.',
+				'PERMISSIONS_WELCOME' : 'Welcome to Overwatch League.',
 
 				'PERMISSIONS_PROMPT': 'In order to get match times in your local time I need permission to access your device information and location.\
 				 Use the Alexa companion app and enable permissions by either selecting the manage permissions link on the most recent card,\
@@ -28,8 +30,11 @@ module.exports = {
 				'HELP_MSG' : 'To get match date and times for a specific team, you could ask, when is the next %s game. To get\
 				 the next game regardless of the team, you could say tell me the next match, or, when is the next game. For\
 				 standings information, you can ask, who are the top teams, and I will tell you the current top three teams.\
-				 You can also ask for any number of teams like the top four or six teams. If you would like to know the current\
-				 standings for every team, you can ask for all standings.',
+				 You may also ask for any number of teams like the top four or six teams. I am also able to tell you the current\
+				 record for any team. If you would like to know the current standings for every team, you can ask for all standings.\
+				 I am also able to tell you today\'s games, yesterday\'s results, and, tomorrow\'s games.',
+
+				 'HELP_REPROMPT' : 'Say help to hear this again.',
 
 				'SIMPLE_REPROMPT' : "Is there something else you would like to know?",
 				'SHUTDOWN_MSG' : 'The world could always use more heros.'
@@ -39,12 +44,14 @@ module.exports = {
 			'translation' : {
 				'SKILL_NAME' : 'Overwatch League',
 				'WELCOME_MSG' : 'Welcome to Overwatch League. You can ask me about Overwatch League games and standings. For example,\
-				 you can ask when the next %s game is, or, who the top three teams are.',
+				 you can ask when the next %s game is, or, who the top three teams are. You can also ask me what games are today, or,\
+				 what yesterday\'s results were. I am also able to tell you the games that will be played tomorrow. For more help,\
+				 say help.',
 
 				'WELCOME_REPROMPT' : 'For match information say, when is the next %s game, or, for ranking information ask what are\
-				 the league standings.',
+				 the league standings. For more help, say help.',
 
-				'PERMISSIONS_WELCOME' : 'Welcome to Overwatch League. You have asked about a next match.',
+				'PERMISSIONS_WELCOME' : 'Welcome to Overwatch League.',
 
 				'PERMISSIONS_PROMPT': 'In order to get match times in your local time I need permission to access your device information and location.\
 				 Use the Alexa companion app and enable permissions by either selecting the manage permissions link on the most recent card,\
@@ -62,8 +69,11 @@ module.exports = {
 				'HELP_MSG' : 'To get match date and times for a specific team, you could ask, when is the next %s game. To get\
 				 the next game regardless of the team, you could say tell me the next match, or, when is the next game. For\
 				 standings information, you can ask, who are the top teams, and I will tell you the current top three teams.\
-				 You can also ask for any number of teams like the top four or six teams. If you would like to know the current\
-				 standings for every team, you can ask for all standings.',
+				 You may also ask for any number of teams like the top four or six teams. I am also able to tell you the current\
+				 record for any team. If you would like to know the current standings for every team, you can ask for all standings.\
+				 I am also able to tell you today\'s games, yesterday\'s results, and, tomorrow\'s games.',
+
+				 'HELP_REPROMPT' : 'Say help to hear this again.',
 
 				'SIMPLE_REPROMPT' : "Is there something else you would like to know?",
 				'SHUTDOWN_MSG' : 'The world could always use more heros.'
