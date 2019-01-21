0770b83f2b4405429de83db47f098822881e21ba
Mitch Burnett <euphamisim@gmail.com>
2019-01-21 10:59:40 -0700

    Extend deserializer and Player model
    Fixed some bugs with deserializing lists of objects. Also needed to add an
    intermediate class PlayerInfo to add to the players since the OWL API responds
    with the data in that particular structure. Given that the OWL API may return
    more than we want/need we may want to consider some way around being able to
    filter thorugh ther response. Specifically, it would have been nice to just have
    a list of owl_model.player.Player's and that each player had all of his metadata
    data as a top level attribute. However, while the teams players attribute is
    still a lsit of Players's each player has an info attribute that is of type
    owl_model.player.PlayerInfo.

--- lambda/py/owl_model/player.py
+++ lambda/py/owl_model/player.py
@@ -2,25 +2,24 @@
 
 from modelobject import ModelObject
 
-class Player(ModelObject):
+class PlayerInfo(ModelObject):
     """
-    An OWL player
     """
     cls_attr_types = {
         'id': 'str',
         'name': 'str',
         'hometown': 'str',
         'firstname': 'str',
-        'lastname' : 'str',
-        'metadata' : 'dict()'
+        'lastname' : 'str'#,
+        #'metadata' : 'dict(str, owl_model.hero.Heros)'
     }
     cls_attr_map = {
         'id': 'id',
         'name': 'name',
         'hometown': 'homeLocation',
         'firstname': 'givenName',
-        'lastname': 'familyName',
-        'metadata': 'attributes'
+        'lastname': 'familyName'#,
+        #'metadata': 'attributes'
     }
 
 
@@ -33,6 +32,27 @@ class Player(ModelObject):
         self.hometown = hometown
         self.firstname = firstname
         self.lastname = lastname
+        self.metadata = metadata
+
+
+class Player(PlayerInfo):
+    """
+    An OWL player
+    """
+    cls_attr_types = {
+        'team': 'dict(str, str)',
+        'info': 'owl_model.player.PlayerInfo'
+        #'flags': 'object'
+    }
+    cls_attr_map = {
+        'team': 'team',
+        'info': 'player'
+        #'metadata': 'flags'
+    }
+
+    def __init__ (self, team=None, info=None, metadata=None):
+        self.team = team
+        self.info = info
         self.metadata = metadata
 
 
