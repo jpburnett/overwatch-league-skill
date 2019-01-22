d80d17cd208665935ce60fd29e722656b627b72e
Mitch Burnett <euphamisim@gmail.com>
2019-01-22 08:05:07 -0700

    Extend Model Definitions
    Looked at a response from the /teams endpoint and extended the model to include
    many of the attributes we would possibly be interested in now and in the future.
    
    Began to add a owl_model.URL class. The idea here was that the URL class when
    instantiated could go and fetch the contatct or have a wrapper method that could
    handle the fetch of data without the user having to explicitly making the
    request. AS of now the URL class is a place holder as there is no model defined
    or way to handle it in the deserializer because mapping a URL to all the
    different names that could be defined in the response from OWL (e.g., icon,
    logo, accounts, twitter, etc.) needs to be figured out.

--- lambda/py/owl_model/team.py
+++ lambda/py/owl_model/team.py
@@ -13,19 +13,44 @@ class Team(ModelObject):
     # wrong builtin type did not phase it.
         'id': 'str',
         'name': 'str',
-        'players': 'list[owl_model.player.Player]'
+        'players': 'list[owl_model.player.Player]',
+        'division': 'str',
+        'hometown': 'str',
+        'country': 'str',
+        'shortname': 'str',
+        'logo': 'owl_model.URL',
+        'icon': 'owl_model.URL',
+        'colorA': 'str',
+        'colorB': 'str'
     }
     cls_attr_map = {
         'id': 'id',
         'name': 'name',
-        'players': 'players'
+        'players': 'players',
+        'division': 'owl_division',
+        'hometown': 'homeLocation',
+        'country': 'addressCountry',
+        'shortname': 'abbreviatedName',
+        'logo': 'logo',
+        'icon': 'icon',
+        'colorA': 'primaryColor',
+        'colorB': 'secondaryColor'
     }
 
 
-    def __init__ (self, id=None, name=None, players=None):
+    def __init__ (self, id=None, name=None, players=None, division=str,
+                hometown=None, country=None, shortname=None, logo=None,
+                icon=None, colorA=None, colorB=None):
         self.id = id
         self.name = name
         self.players = players
-
+        self.division = division
+        self.hometown = hometown
+        self.country = country
+        self.shortname = shortname
+        self.logo = logo
+        self.icon = icon
+        self.colorA = colorA
+        self.colorB = colorB
 
 
