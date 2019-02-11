d8796860023e44819aac835670f4de13b3462f20
Mitch Burnett <euphamisim@gmail.com>
2019-02-10 23:08:51 -0700

    APIRequest ready for use
    After messing around with the APIRequest class it is in a state that is usaable
    and I feel pretty trasparent.
    
    To overcome many of the design questions the end result is that the APIRequest
    is the umbrella factory class with classmethods that are the factory methods
    making the api calls and returning objects from the owl_model library.
    
    The main block of apirequest.py has the beginning of some examples for how to
    use the APIRequest class and how to begin to utilize the information from the
    deserializer.

--- lambda/py/owl_model/apirequest.py
+++ lambda/py/owl_model/apirequest.py
@@ -2,106 +2,101 @@
 from owl_model.url import URL
 from utils.deserializer import SerDeser
 
+from owl_model.easteregg import EasterEgg
 from owl_model.team import Team
 from owl_model.league import League
 
-#TODO: make note in different request class about the difference between making
+# TODO: make note in different request class about the difference between making
 # requests at teams and teamsById and schedule and matchById. For example, when
 # making a request at schedule it includes stages infromation and the individual
 # matches do not have the list of players in the match object. But a call at the
 # matchById endpoint includes much more information. Similar thing was observed
 # for teams but need to go back and note and document that.
 
-#TODO: should 'bootstrap_subclass()' and 'get_resource()' be hidden in the
-# implementation somehow? Because although calling them doesn't 'break' anything
-# it shouldn't really be explored
-
-#TODO: should this extend url? How to make this class the best it can be is
+# TODO: should this extend url? How to make this class the best it can be is
 # still a little fuzzy to me
 class APIRequest(URL):
     """
     """
     baseurl = 'https://api.overwatchleague.com'
     endpoints = {
-        'APIRequest': '',
         'TeamsRequest': '/teams',
         'TeamByIdRequest': '/team/{}',
         'schedule': '/schedule',
         'matchById': '/match/{}',
         'rankings': '/ranking'
     }
+    
+    path = None
+    deser_cls_type = None
+    sd = SerDeser()
 
-    cls_attr_types = {
-        'theWorld': 'str'
-    }
-    cls_attr_map = {
-        'theWorld': 'the world'
-    }
-
-    # TODO: the 'path' method was first here in the class __init__ method
-    # but I then realized that this is a chicken and the egg problem since
-    # in the deserializer the object is created. So the makeapicall class
-    # method was instead made. The todo here is determine if this is the
-    # final approach we should take
 
     @classmethod
-    def makeapicall(cls):
-        # TODO: still thinking if the class should use 'path' as the variable
-        cls.path = cls.baseurl + cls.endpoints[cls.__name__]
-        sd = SerDeser()
-        return sd.deserialize(cls.get_resource(cls), cls)
-
-
-    def __init__ (self, theWorld=None):
+    def __makecall(cls):
         """
-        API request at the default endpoint
+        Make the call to the OWL API. Fetch the content and pass to the deserializer.
         """
-        self.theWorld = theWorld
+        return cls.sd.deserialize(cls.get_resource(cls), cls.deser_cls_type)
 
 
-class TeamByIdRequest(APIRequest):
-    """
-    API request for a specifc team by ID
-    """
-    cls_attr_types = {
-    }
-    cls_attr_map = {
-    }
-
-    # TODO: would be convenient to allow making requests by teamname (as well as
-    # an id) since people don't remember team id's off the top of their head
     @classmethod
-    def makeapicall(cls, teamid=None):
-        """
-        """
-        cls.path = cls.baseurl + cls.endpoints[cls.__name__]
+    def easteregg(cls):
+        cls.path = cls.baseurl;
+        cls.deser_cls_type = EasterEgg
+        return cls.__makecall()
+
+
+    @classmethod
+    def teams(cls):
+        cls.path = cls.baseurl + cls.endpoints['TeamsRequest']
+        cls.deser_cls_type = League
+        return cls.__makecall()
+
+
+    @classmethod
+    def teamfromid(cls, teamid=None):
+        if not teamid:
+            print("ERROR: team id not provided")
+            return None
+
+        cls.path = cls.baseurl + cls.endpoints['TeamByIdRequest']
         cls.path = cls.path.format(teamid)
-        sd = SerDeser()
-        return sd.deserialize(cls.get_resource(cls), Team)
+        cls.deser_cls_type = Team
+        return cls.__makecall()
 
 
-    def __init__ (self):
-        """
-        """
-        pass
+if __name__ == "__main__":
 
+    # Fetch the entire collections of teams in the OWL from the '/teams'
+    # endpoint
+    league = APIRequest.teams() 
 
-class TeamsRequest(APIRequest):
-    """
-    API request to the teams endpoing fetching all the teams in the league
-    """
-    cls_attr_types = {
-    }
-    cls_attr_map = {
-    }
+    # Get a specifc team by teamid (summary of team id's can be found in
+    # owl_model.league.League)
+    team = APIRequest.teamfromid('4523')
 
-    @classmethod
-    def makeapicall(cls):
-        cls.path=cls.baseurl + cls.endpoints[cls.__name__]
-        sd = SerDeser()
-        return sd.deserialize(cls.get_resource(cls), League)
+    # The APIRequest classmethod are a factory constructor returning model
+    # objects from owl_model. The above call to get team with id 4523 (the
+    # Dallas Fuel) returns a owl_model.team.Team and the Team model object has
+    # several attribute to expose relevant for a team such as the players of the
+    # team or the schedule of the team.
 
-    def __init__ (self):
-        """
-        """
-        pass
+    # See owl_model.team.Team for more detail on available information. The
+    # following show some examples for how to use a Team
+
+    # show the info for the first player on the team.
+    print(team.players[0].info, "\n")
+
+    # Print all the players on the team
+    print("The players for the", team.name, "are:")
+    for player in team.players:
+        print("\t", player.info.name)
+
+    # Get the date for the first match the team has and timezone for the match.
+    # The team schedule is a list of owl_model.match.Match. See
+    # owl_model/model.py for more detail.
+    print(team.schedule[0].startdate, team.schedule[0].timezone, "\n")
+
+    # and remeber
+    print("The world,", APIRequest.easteregg().theWorld)
